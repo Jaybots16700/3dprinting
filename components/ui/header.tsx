ce9b9ec310cf99e4b441e968f44a28469ef6700c
matthewglasser <matthewsglasser@gmail.com>
2024-07-05 19:30:13 -0400

    header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <div className="w-full h-12 bg-slate-700"></div>;
+  return (
+    <div className="w-full flex h-16 items-center px-5 bg-slate-700">
+      <Link
+        href="https://jaybots.org"
+        className="w-10 flex items-center text-xl space-x-4 h-10"
+      >
+        <Image
+          src="/images/logo.png"
+          alt="Jaybots logo"
+          className="h-full w-full"
+          width={40}
+          height={40}
+        />
+        <span>Jaybots</span>
+      </Link>
+    </div>
+  );
 }
