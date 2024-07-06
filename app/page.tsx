67d74381787bec4dd49eba20b969d5fd0da28fe8
matthewglasser <matthewsglasser@gmail.com>
2024-07-05 20:10:30 -0400

    order btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 "></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <Link
+        href="/order"
+        className="bg-blue-900 text-2xl py-2 px-5 rounded-lg hover:scale-110 duration-150"
+      >
+        Order!
+      </Link>
+    </main>
   );
 }
