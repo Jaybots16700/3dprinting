6600811988301a668a98d87dd1891e0e9bb1a281
matthewglasser <matthewsglasser@gmail.com>
2024-07-05 20:15:09 -0400

    prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <Link
-        href="/order"
-        className="bg-blue-900 text-2xl py-2 px-5 rounded-lg hover:scale-110 duration-150"
-      >
-        Order!
-      </Link>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Link href="/order" className="rounded-lg bg-blue-900 px-5 py-2 text-2xl duration-150 hover:scale-110">
+				Order!
+			</Link>
+		</main>
+	);
 }
