ea82b4af33561ddb936869c3ac8665bb1c6d9a34
matthewglasser <matthewsglasser@gmail.com>
2024-07-06 14:45:26 -0400

    begin disclosure

--- app/(app)/admin/dashboard.tsx
+++ app/(app)/admin/dashboard.tsx
@@ -3,7 +3,15 @@
 import { Badge } from "@/components/badge";
 import { getAllOrders, updateStatus } from "@/lib/serverActions";
 import { PartOrder, OrderStatus } from "@/types";
-import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react";
+import {
+	Disclosure,
+	DisclosureButton,
+	DisclosurePanel,
+	Listbox,
+	ListboxButton,
+	ListboxOption,
+	ListboxOptions,
+} from "@headlessui/react";
 import { LinkIcon } from "@heroicons/react/24/solid";
 import { WithId } from "mongodb";
 import Link from "next/link";
@@ -21,45 +29,46 @@ export default function AdminDashboard() {
 			<h1 className="mb-24 text-6xl font-semibold">Admin Dashboard</h1>
 			<div className="mb-16 w-full max-w-3xl space-y-4">
 				{orders.map((order, index) => (
-					<div
-						key={order.link}
-						className="flex h-10 w-full items-center justify-between rounded-xl border border-slate-700 bg-zinc-800 px-2">
-						<div className="flex items-center space-x-2 pl-1">
-							<Link href={order.link}>
-								<LinkIcon className="size-4 text-gray-400 duration-150 hover:text-white" />
-							</Link>
-							<span>{order.partName}</span>
-						</div>
-						<Listbox
-							value={order.status}
-							onChange={async (newStatus) => {
-								await updateStatus(order._id.toString(), newStatus);
-								setOrders((prev) => [
-									...prev.slice(0, index),
-									{ ...prev[index], status: newStatus },
-									...prev.slice(index + 1),
-								]);
-							}}>
-							<ListboxButton className="flex outline-none">
-								<Badge status={order.status} />
-							</ListboxButton>
-							<ListboxOptions
-								anchor="bottom"
-								transition
-								className="flex translate-y-1 flex-col items-center space-y-1 rounded-xl border border-zinc-700 bg-zinc-800 p-2 transition duration-150 ease-in [--anchor-gap:var(--spacing-1)] focus:outline-none data-[leave]:data-[closed]:opacity-0">
-								{["received", "queued", "printing", "awaiting payment", "completed", "delivered"]
-									.filter((s) => s !== order.status)
-									.map((status) => (
-										<ListboxOption
-											key={status}
-											value={status}
-											className="flex w-full cursor-pointer items-center justify-center rounded p-1 duration-150 hover:bg-zinc-700/50">
-											<Badge status={status as OrderStatus} />
-										</ListboxOption>
-									))}
-							</ListboxOptions>
-						</Listbox>
-					</div>
+					<Disclosure as="div" key={order._id.toString()}>
+						<DisclosureButton className="flex h-10 w-full items-center justify-between rounded-xl border border-slate-700 bg-zinc-800 px-2 data-[open]:rounded-b-none">
+							<div className="flex items-center space-x-2 pl-1">
+								<Link href={order.link} target="_blank">
+									<LinkIcon className="size-4 text-gray-400 duration-150 hover:text-white" />
+								</Link>
+								<span>{order.partName}</span>
+							</div>
+							<Listbox
+								value={order.status}
+								onChange={async (newStatus) => {
+									await updateStatus(order._id.toString(), newStatus);
+									setOrders((prev) => [
+										...prev.slice(0, index),
+										{ ...prev[index], status: newStatus },
+										...prev.slice(index + 1),
+									]);
+								}}>
+								<ListboxButton className="flex outline-none">
+									<Badge status={order.status} />
+								</ListboxButton>
+								<ListboxOptions
+									anchor="bottom"
+									transition
+									className="flex translate-y-1 flex-col items-center space-y-1 rounded-xl border border-zinc-700 bg-zinc-800 p-2 transition duration-150 ease-in [--anchor-gap:var(--spacing-1)] focus:outline-none data-[leave]:data-[closed]:opacity-0">
+									{["received", "queued", "printing", "awaiting payment", "completed", "delivered"]
+										.filter((s) => s !== order.status)
+										.map((status) => (
+											<ListboxOption
+												key={status}
+												value={status}
+												className="flex w-full cursor-pointer items-center justify-center rounded p-1 duration-150 hover:bg-zinc-700/50">
+												<Badge status={status as OrderStatus} />
+											</ListboxOption>
+										))}
+								</ListboxOptions>
+							</Listbox>
+						</DisclosureButton>
+						<DisclosurePanel className="flex h-64 rounded-b-xl border border-t-0 border-slate-700 bg-zinc-800"></DisclosurePanel>
+					</Disclosure>
 				))}
 			</div>
 		</div>
