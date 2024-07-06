c5b93ef3a343dd0df76f1c8cf6bc65017a2eb1f9
matthewglasser <matthewsglasser@gmail.com>
2024-07-05 20:51:13 -0400

    new order form

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -8,31 +8,60 @@ export default function Order() {
 			<h1 className="text-6xl font-semibold">Place an Order</h1>
 			<span className="mt-4 font-light text-slate-300">Pricing: ${pricePerGram} per gram</span>
 
-			<form className="mt-12 w-full max-w-3xl space-y-8 rounded-3xl border border-slate-700 px-8 py-6">
-				<div className="space-y-2">
-					<span className="text-2xl">Name</span>
-					<input
-						type="text"
-						className="block w-96 rounded border border-white/10 bg-white/5 px-3 py-1 outline-none"
-						placeholder="First Name"
-						required
-					/>
-					<input
-						type="text"
-						className="block w-96 rounded border border-white/10 bg-white/5 px-3 py-1 outline-none"
-						placeholder="Last Name"
-						required
-					/>
+			<form className="mx-auto mt-12 w-full max-w-lg rounded-2xl border border-slate-800 p-8">
+				<div className="grid md:grid-cols-2 md:gap-6">
+					<div className="group relative z-0 mb-5 w-full">
+						<input
+							type="text"
+							name="floating_first_name"
+							id="floating_first_name"
+							className="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm text-white focus:border-blue-500 focus:outline-none focus:ring-0"
+							placeholder=" "
+							required
+						/>
+						<label
+							htmlFor="floating_first_name"
+							className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500 rtl:peer-focus:translate-x-1/4">
+							First name
+						</label>
+					</div>
+					<div className="group relative z-0 mb-5 w-full">
+						<input
+							type="text"
+							name="floating_last_name"
+							id="floating_last_name"
+							className="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm text-white focus:border-blue-500 focus:outline-none focus:ring-0"
+							placeholder=" "
+							required
+						/>
+						<label
+							htmlFor="floating_last_name"
+							className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500 rtl:peer-focus:translate-x-1/4">
+							Last name
+						</label>
+					</div>
 				</div>
-				<div className="space-y-2">
-					<span className="text-2xl">Email</span>
+				<div className="group relative z-0 mb-5 w-full">
 					<input
 						type="email"
-						className="block w-96 rounded border border-white/10 bg-white/5 px-3 py-1 outline-none"
-						placeholder="3dprinting@jaybots.org"
+						name="floating_email"
+						id="floating_email"
+						className="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm text-white focus:border-blue-500 focus:outline-none focus:ring-0"
+						placeholder=" "
 						required
 					/>
+					<label
+						htmlFor="floating_email"
+						className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4">
+						Email address
+					</label>
 				</div>
+
+				<button
+					type="submit"
+					className="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-800 sm:w-auto">
+					Submit
+				</button>
 			</form>
 		</div>
 	);
