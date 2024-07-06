3668947153f99a503008d81b85c6dc628aff495c
matthewglasser <matthewsglasser@gmail.com>
2024-07-06 15:15:27 -0400

    filament input

--- app/(app)/admin/dashboard.tsx
+++ app/(app)/admin/dashboard.tsx
@@ -104,6 +104,26 @@ export default function AdminDashboard() {
 									)}
 								</div>
 							</div>
+
+							<Divider />
+
+							<div className="flex w-full justify-between">
+								<div className="group relative z-0 mt-1 w-full">
+									<input
+										type="text"
+										name="filament"
+										value={order.filament}
+										// onChange={(e) => setOther(e.target.value)}
+										className="peer block w-full appearance-none border-0 border-b-2 border-gray-600 bg-transparent px-0 py-2.5 text-sm text-white focus:border-blue-500 focus:outline-none focus:ring-0"
+										placeholder=" "
+									/>
+									<label
+										htmlFor="filament"
+										className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-500 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4">
+										Material (g)
+									</label>
+								</div>
+							</div>
 						</DisclosurePanel>
 					</Disclosure>
 				))}
